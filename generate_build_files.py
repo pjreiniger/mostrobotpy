b45eebb343ae708a7d606000b526cc6f85554e9b
PJ Reiniger <pj.reiniger@gmail.com>
2024-07-24 03:49:39 -0400

    More testing

--- generate_build_files.py
+++ generate_build_files.py
@@ -6,7 +6,44 @@ from pathlib import Path
 
 
 
-def write_toplevel(setup, project_root):
+def write_toplevel(setup, project_root, wrappers):
+    print("Writing top level...")
+
+    gen_internal_proj_deps = ""
+    gen_python_deps = ""
+    pybind_deps = None
+    pkgcfg_deps = ""
+
+
+    seen_deps = set()
+
+    for wrapper in wrappers:
+        print("--Wrapper", wrapper.package_name)
+        if pybind_deps is None:
+            pybind_deps = "deps = [\n"
+            gen_internal_proj_deps += 'internal_project_dependencies = ['
+            gen_python_deps = "python_deps = ["
+            pkgcfg_deps += "deps = ["
+
+        if wrapper.depends:
+            pass
+
+            for dep in wrapper.depends:
+                if dep in seen_deps:
+                    continue
+                gen_internal_proj_deps += f'"{dep}",'
+                pybind_deps += f'        "//subprojects/robotpy-{dep}:{dep}",'
+                gen_python_deps += f'"//subprojects/robotpy-{dep}:pkgcfg",\n'
+                pkgcfg_deps += f'"//subprojects/robotpy-{dep}:pkgcfg",\n'
+                seen_deps.add(dep)
+
+    if pybind_deps:
+        pybind_deps += "]"
+        gen_internal_proj_deps += '],\n'
+        gen_python_deps += '],\n'
+        pkgcfg_deps += '],\n'
+
+
     contents = f"""load("@rules_python//python:defs.bzl", "py_library")
 load("//rules_robotpy_utils:generate_robotpy_source_files.bzl", "generate_robotpy_source_files")
 load("//rules_robotpy_utils:pybind_rules.bzl", "generated_files_helper", "pybind_python_library")
@@ -26,7 +63,7 @@ generate_robotpy_source_files(
     name = "{setup.project.base_package}",
     config_file = ":config_file",
     headers = ["@bazelrio_edu_wpi_first_{setup.project.base_package}_{setup.project.base_package}-cpp_headers//:header_files"],
-)
+{gen_internal_proj_deps}{gen_python_deps})
 
 pybind_python_library(
     name = "{setup.project.base_package}",
@@ -37,7 +74,7 @@ pybind_python_library(
     ],
     imports = ["."],
     visibility = ["//visibility:public"],
-)
+{pybind_deps})
 
 py_library(
     name = "pkgcfg",
@@ -46,8 +83,7 @@ py_library(
     ],
     imports = ["."],
     visibility = ["//visibility:public"],
-)
-
+{pkgcfg_deps})
 """
     
     with open(project_root / "BUILD.bazel", 'w') as f:
@@ -56,16 +92,48 @@ py_library(
 
 def generate_subproject(setup, project_root, wrapper):
 
-    gen_deps = ""
+    gen_internal_proj_deps = ""
+    gen_python_deps = ""
     pybind_deps = ""
     if wrapper.depends:
-        pybind_deps += "\n"
-        pybind_deps += '        "//subprojects/robotpy-wpiutil/wpiutil:wpiutil_pybind_library",'
+        pybind_deps += "deps = [\n"
+        gen_internal_proj_deps += 'internal_project_dependencies = ['
+        gen_python_deps = "python_deps = ["
+        # pkgcfg_deps += "deps = ["
 
-        gen_deps += '    internal_project_dependencies = ["wpiutil"],\n'
-        gen_deps += '    python_deps = [\n'
-        gen_deps += '        "//subprojects/robotpy-wpiutil:pkgcfg",\n'
-        gen_deps += '    ],\n'
+        for dep in wrapper.depends:
+            print(dep, type(dep))
+            gen_internal_proj_deps += f'"{dep}",'
+        #     pybind_deps += f'        "//subprojects/robotpy-{dep}:{dep}",'
+            gen_python_deps += f'"//subprojects/robotpy-{dep}:pkgcfg",\n'
+        #     # pkgcfg_deps += f'"//subprojects/robotpy-{dep}:pkgcfg",\n'
+
+        pybind_deps += "]"
+        gen_internal_proj_deps += '],\n'
+        gen_python_deps += '],\n'
+        # pkgcfg_deps += '],\n'
+        # pybind_deps += "\n"
+        # pybind_deps += '        "//subprojects/robotpy-wpiutil/wpiutil:wpiutil_pybind_library",'
+
+        # gen_deps += '    internal_project_dependencies = ["wpiutil"],\n'
+        # gen_deps += '    python_deps = [\n'
+        # gen_deps += '        "//subprojects/robotpy-wpiutil:pkgcfg",\n'
+        # gen_deps += '    ],\n'
+
+
+    entry_point = None
+    extra_srcs = ""
+    for src in wrapper.cfg.sources:
+        print(wrapper.package_name)
+        stripped_src = src.removeprefix(f"{wrapper.package_name}/")
+        if src.endswith("main.cpp") or src.endswith(f"{wrapper.package_name}.cpp"):
+            entry_point = stripped_src
+        else:
+            extra_srcs += "        \"" + stripped_src + "\",\n"
+        print(src, " -> ", stripped_src)
+
+    if extra_srcs:
+        extra_srcs = "    extra_srcs = [\n" + extra_srcs + "\n    ],\n"
 
     contents = f"""load("@rules_cc//cc:defs.bzl", "cc_library")
 load("//rules_robotpy_utils:copy_native_file.bzl", "copy_native_file")
@@ -75,27 +143,26 @@ load("//rules_robotpy_utils:pybind_rules.bzl", "create_pybind_library")
 generate_robotpy_project_files(
     name = "{wrapper.package_name}",
     config_file = "//subprojects/{setup.pypi_package}:config_file",
-{gen_deps}    visibility = ["//subprojects/{setup.pypi_package}:__subpackages__"],
+{gen_internal_proj_deps}{gen_python_deps}    visibility = ["//subprojects/{setup.pypi_package}:__subpackages__"],
 )
 
 
 create_pybind_library(
     "{wrapper.package_name}",
-    entry_point = ["src/main.cpp"],
+    entry_point = ["{entry_point}"],
     extension_visibility = ["//subprojects/{setup.pypi_package}:__subpackages__"],
-    generation_helper_prefix = "//subprojects/{setup.pypi_package}:{wrapper.package_name}",
-    deps = [{pybind_deps}
+{extra_srcs}    generation_helper_prefix = "//subprojects/{setup.pypi_package}:{wrapper.package_name}",
+    deps = [
         "@bzlmodrio-allwpilib//libraries/cpp/{wrapper.package_name}",
     ],
 )
-
 
 alias(
     name = "TEMP_shared_library",
     actual = select({{
         "@bazel_tools//src/conditions:darwin": "@bazelrio_edu_wpi_first_{wrapper.package_name}_{wrapper.package_name}-cpp_osxuniversal//:shared_libs",
         "@rules_bazelrio//conditions:windows": "@bazelrio_edu_wpi_first_{wrapper.package_name}_{wrapper.package_name}-cpp_windowsx86-64//:shared_libs",
-        "//conditions:default":                "@bazelrio_edu_wpi_first_{wrapper.package_name}_{wrapper.package_name}-cpp_linuxx86-64//:shared",
+        "//conditions:default": "@bazelrio_edu_wpi_first_{wrapper.package_name}_{wrapper.package_name}-cpp_linuxx86-64//:shared",
     }}),
 )
 
@@ -106,10 +173,9 @@ copy_native_file(
 
 filegroup(
     name = "python_files",
-    srcs = glob(["*.py"]),
+    srcs = glob(["**/*.py"]),
     visibility = ["//subprojects/{setup.pypi_package}:__subpackages__"],
 )
-
 """
 
     output_file = project_root / wrapper.package_name.replace(".", "/") / "BUILD.bazel"
@@ -124,7 +190,7 @@ def generate_project_build_files(project_root):
     setup = Setup(config, ".", [])
     # print(setup.project.base_package)
 
-    write_toplevel(setup, project_root)
+    write_toplevel(setup, project_root, setup.wrappers)
     
     for wrapper in setup.wrappers:
         print(wrapper.package_name)
@@ -137,16 +203,16 @@ def generate_project_build_files(project_root):
 
 def main():
     SUBPROJECTS = [
-        "pyntcore",
-        "robotpy-apriltag",
-        "robotpy-cscore",
-        "robotpy-hal",
-        "robotpy-halsim-ds-socket",
+        # "pyntcore",
+        # "robotpy-apriltag",
+        # "robotpy-cscore",
+        # "robotpy-hal",
+        # "robotpy-halsim-ds-socket",
         "robotpy-halsim-gui",
-        "robotpy-halsim-ws",
-        "robotpy-wpimath",
-        "robotpy-wpinet",
-        "robotpy-wpiutil",
+        # "robotpy-halsim-ws",
+        # "robotpy-wpimath",
+        # "robotpy-wpinet",
+        # "robotpy-wpiutil",
     ]
 
     for p in SUBPROJECTS:
