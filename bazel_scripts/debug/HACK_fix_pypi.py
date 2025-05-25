276255ed19578dca01c75cc5cc488250ae52bccf
PJ Reiniger <pj.reiniger@gmail.com>
2025-05-25 03:53:45 -0400

    Try again

---
+++ bazel_scripts/debug/HACK_fix_pypi.py
@@ -0,0 +1,11 @@
+
+
+with open("bazel-mostrobotpy/external/rules_python~~pip~allwpilib_pip_deps_310_semiwrap/site-packages/semiwrap/pyproject.py", 'r') as f:
+    contents = f.read()
+
+print(contents)
+
+contents = contents.replace("def package_root(self) -> pathlib.Path:", "def package_root(self) -> pathlib.Path:\n        self._package_root = self.root")
+
+with open("bazel-mostrobotpy/external/rules_python~~pip~allwpilib_pip_deps_310_semiwrap/site-packages/semiwrap/pyproject.py", 'w') as f:
+    f.write(contents)
